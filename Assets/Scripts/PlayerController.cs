5de9df97c938abed150a85511d4b249187fb89d4
Dakota <dakotacbrown@gmail.com>
2020-06-13 11:54:47 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+
+
+    private PlayerControls playerControls;
+    private Rigidbody2D rb;
+    [SerializeField] private float playerSpeed = 4f;
+    private float xLeftInput, yLeftInput;
+    private float xRightInput, yRightInput;
+    private Vector2 rightInput;
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void OnDisable() {
+        playerControls.Disable();
+
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        GetPlayerInput();
+
+
+    }
+
+    //Read the movement value
+    private void GetPlayerInput() {
+        xLeftInput = playerControls.Movement.Horizontal.ReadValue<float>();
+        yLeftInput = playerControls.Movement.Vertical.ReadValue<float>();
+
+        xRightInput = playerControls.Movement.HorizontalRotation.ReadValue<float>();
+        yRightInput = playerControls.Movement.VerticalRotation.ReadValue<float>();
+
+        rightInput = new Vector2(xRightInput, yRightInput);
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector2 currentMovement = new Vector2(xLeftInput * playerSpeed * Time.deltaTime, yLeftInput * playerSpeed * Time.deltaTime);
+        rb.MovePosition(rb.position + currentMovement);
+
+        Vector3 currentRotation = Vector3.left * rightInput.x + Vector3.down * rightInput.y;
+        Quaternion playerRotation = Quaternion.LookRotation(currentRotation, Vector3.forward);
+
+        rb.SetRotation(playerRotation);
+    }
+
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,373 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Movement"",
+            ""id"": ""a1825df5-26a3-46e6-a8d1-935603e7ae1b"",
+            ""actions"": [
+                {
+                    ""name"": ""Horizontal"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""78402496-7e22-45cd-85b7-8b47c1877637"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Vertical"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""5d8dde87-1f2f-4b64-a00f-ee72fc4a286a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Horizontal Rotation"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""46d81ab9-1151-493e-9ef6-852658de6312"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Vertical Rotation"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""f0b4d243-0aa7-4fe6-9126-c0ed3de3f1ed"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""f550c423-b7ee-49d1-a1e1-8feb2f108a1d"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""2a890121-4e0a-4c55-b7b0-57537edbd19a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""8665b5b5-069f-4259-895c-03713c3955ec"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Generic Controller"",
+                    ""id"": ""29637da1-43fc-459f-a127-5fd04f92a694"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""ff21bd0d-573f-46ca-b7aa-83dcb1ded6ad"",
+                    ""path"": ""<Gamepad>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""624758a2-b01d-4700-84f3-4a870e75a38d"",
+                    ""path"": ""<Gamepad>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""d7c46d83-9184-4067-8eb1-34a4a97aa767"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""b7f024e0-3987-40a8-8879-d546bc03d0fe"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""c43b78b6-9dd7-4ceb-bb38-fb48c26ed53d"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Generic Controller"",
+                    ""id"": ""b9060ec0-573d-4ab4-96d3-f9c83f069109"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""f4e3d78b-dacd-4841-a715-acac74397169"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""339c476d-0b96-459b-b37b-920060d92809"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""a4cc234f-31fd-4ea5-8ecb-b5ba03dab3b5"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal Rotation"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""e29a90f5-af15-4539-af60-f35999f98ecc"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""f75e1bc0-596c-470e-9fc6-fd1a2d4ac708"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Horizontal Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""6ddb479c-c91e-4d94-ae77-f63b331d3c4d"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical Rotation"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""ed84ca34-6208-4ce1-b3c2-42915f8e5f23"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""82a8f747-b1f5-4f6a-9713-16409edbfda0"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Vertical Rotation"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Movement
+        m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
+        m_Movement_Horizontal = m_Movement.FindAction("Horizontal", throwIfNotFound: true);
+        m_Movement_Vertical = m_Movement.FindAction("Vertical", throwIfNotFound: true);
+        m_Movement_HorizontalRotation = m_Movement.FindAction("Horizontal Rotation", throwIfNotFound: true);
+        m_Movement_VerticalRotation = m_Movement.FindAction("Vertical Rotation", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Movement
+    private readonly InputActionMap m_Movement;
+    private IMovementActions m_MovementActionsCallbackInterface;
+    private readonly InputAction m_Movement_Horizontal;
+    private readonly InputAction m_Movement_Vertical;
+    private readonly InputAction m_Movement_HorizontalRotation;
+    private readonly InputAction m_Movement_VerticalRotation;
+    public struct MovementActions
+    {
+        private @PlayerControls m_Wrapper;
+        public MovementActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Horizontal => m_Wrapper.m_Movement_Horizontal;
+        public InputAction @Vertical => m_Wrapper.m_Movement_Vertical;
+        public InputAction @HorizontalRotation => m_Wrapper.m_Movement_HorizontalRotation;
+        public InputAction @VerticalRotation => m_Wrapper.m_Movement_VerticalRotation;
+        public InputActionMap Get() { return m_Wrapper.m_Movement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MovementActions set) { return set.Get(); }
+        public void SetCallbacks(IMovementActions instance)
+        {
+            if (m_Wrapper.m_MovementActionsCallbackInterface != null)
+            {
+                @Horizontal.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnHorizontal;
+                @Horizontal.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnHorizontal;
+                @Horizontal.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnHorizontal;
+                @Vertical.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnVertical;
+                @Vertical.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnVertical;
+                @Vertical.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnVertical;
+                @HorizontalRotation.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnHorizontalRotation;
+                @HorizontalRotation.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnHorizontalRotation;
+                @HorizontalRotation.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnHorizontalRotation;
+                @VerticalRotation.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnVerticalRotation;
+                @VerticalRotation.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnVerticalRotation;
+                @VerticalRotation.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnVerticalRotation;
+            }
+            m_Wrapper.m_MovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Horizontal.started += instance.OnHorizontal;
+                @Horizontal.performed += instance.OnHorizontal;
+                @Horizontal.canceled += instance.OnHorizontal;
+                @Vertical.started += instance.OnVertical;
+                @Vertical.performed += instance.OnVertical;
+                @Vertical.canceled += instance.OnVertical;
+                @HorizontalRotation.started += instance.OnHorizontalRotation;
+                @HorizontalRotation.performed += instance.OnHorizontalRotation;
+                @HorizontalRotation.canceled += instance.OnHorizontalRotation;
+                @VerticalRotation.started += instance.OnVerticalRotation;
+                @VerticalRotation.performed += instance.OnVerticalRotation;
+                @VerticalRotation.canceled += instance.OnVerticalRotation;
+            }
+        }
+    }
+    public MovementActions @Movement => new MovementActions(this);
+    public interface IMovementActions
+    {
+        void OnHorizontal(InputAction.CallbackContext context);
+        void OnVertical(InputAction.CallbackContext context);
+        void OnHorizontalRotation(InputAction.CallbackContext context);
+        void OnVerticalRotation(InputAction.CallbackContext context);
+    }
+}
